58edc8f008f059977ae6911e72e1360b810a5866
Iamneo <techadmin@iamneo.ai>
2023-10-02 11:33:19 +0000

    Backup Commit

---
+++ springapp/configuration/Main.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the Student bean from the Spring container
+        Student student = (Student) context.getBean("student");
+
+        // Access and print student details
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("First Name: " + student.getFirstName());
+        System.out.println("Last Name: " + student.getLastName());
+    }
+}
